b7b7eb82c3503a923d2db07c6c60bea59b6970db
Martin Infante <maleinf09@gmail.com>
2020-10-23 17:05:57 -0300

    Adds parsing and exporting functions

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QPushButton, QMainWindow, QLabel
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
 from PyQt5.QtWidgets import QFileDialog
+from helpers.parser import Parser
 import os
 import platform
 
@@ -46,6 +47,7 @@ class ControlPanel(QWidget):
         import_layout.addWidget(self.import_button)
         export_layout = QVBoxLayout()
         self.export_button = QPushButton("Export!")
+        self.export_button.clicked.connect(self.export_to_csv)
         self.plot_button = QPushButton("Plot!")
         export_layout.addWidget(self.export_button)
         export_layout.addWidget(self.plot_button)
@@ -61,6 +63,19 @@ class ControlPanel(QWidget):
             self.filename = filename
             self._parse_tcr_file(self.filename)
 
+    def export_to_csv(self):
+        filename, _ = QFileDialog.getSaveFileName(self, "Select CSV file location", os.getcwd(),
+                                                  "*.csv")
+        if filename:
+            csv_file = open(filename, "w")
+            header = "NAME, TIME OFFSET, VALUE, UNIT\n"
+            csv_file.write(header)
+            for data in self.parsed_file:
+                line = "{},{},{},{}\n".format(data[0], data[1], data[2], data[3])
+                csv_file.write(line)
+            csv_file.close()
+
     def _parse_tcr_file(self, filename):
+        parser = Parser(filename)
+        self.parsed_file = parser.parse_file()
         self.import_label.setText(filename.rsplit(SEPARATOR, 1)[1])
-        pass
---
+++ helpers/parser.py
@@ -0,0 +1,49 @@
+import re
+from consts.consts import params
+
+_RE_COMBINE_WHITESPACE = re.compile(r"\s+")
+HEADER = ";"
+OFFSET = 8
+
+
+class Parser():
+
+    def __init__(self, filename):
+        tcr_file = open(filename, "r")
+        self.lines = tcr_file.readlines()
+        self.erase_header(HEADER)
+
+    def erase_header(self, HEADER):
+        i = 0
+        while self.lines[i][0] == HEADER:
+            i = i+1
+        self.lines = self.lines[i:]
+
+    def parse_file(self):
+        parsed_output = []
+        for line in self.lines:
+            line = line.strip("\n")
+            line = _RE_COMBINE_WHITESPACE.sub(" ", line).strip()
+            line = line.split(" ")
+            data_id = line[4]
+            filtered_id = data_id[2:6]
+            parameter = self._hex_to_params(filtered_id)
+            if parameter:
+                ts = line[1]
+                for key in parameter.keys():
+                    name = key
+                    bits = parameter[key]['Bits']
+                    bits = bits.split("-")
+                    rate = parameter[key]['Rate']
+                    offset = parameter[key]['Offset']
+                    measure = parameter[key]['Measure']
+                    data = ""
+                    for bit in reversed(bits):
+                        data = data + line[OFFSET+int(bit)]
+                    data = int("0x"+data, 16)
+                    value = data * float(rate) + offset
+                    parsed_output.append((name, ts, value, measure))
+        return parsed_output
+
+    def _hex_to_params(self, filtered_id):
+        return params.get(filtered_id)
