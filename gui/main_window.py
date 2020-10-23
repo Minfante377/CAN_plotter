17463dd8cf1af82e546b713c1372bdf717f77595
Martin Infante <maleinf09@gmail.com>
2020-10-23 14:11:55 -0300

    Adds browse file dialog

--- gui/main_window.py
+++ gui/main_window.py
@@ -2,6 +2,13 @@ from PyQt5.QtWidgets import QWidget, QPushButton, QMainWindow, QLabel
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
 from PyQt5.QtWidgets import QFileDialog
 import os
+import platform
+
+if platform.system() == "Windows":
+    SEPARATOR = "\\"
+else:
+    SEPARATOR = "/"
+
 
 class MainWindow(QMainWindow):
 
@@ -20,7 +27,7 @@ class MainWindow(QMainWindow):
         self.setGeometry(self.left, self.top, self.width, self.height)
         layout = QVBoxLayout()
         control_panel = ControlPanel()
-        layout.addLayout(control_panel.layout)
+        layout.addWidget(control_panel)
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
@@ -29,8 +36,9 @@ class MainWindow(QMainWindow):
 class ControlPanel(QWidget):
 
     def __init__(self):
-        import_layout = QVBoxLayout()
-        self.import_label = QLabel("Select a file to import")
+        super().__init__()
+        import_layout = QHBoxLayout()
+        self.import_label = QLabel("Select a trc file to import")
         self.import_button = QPushButton("Browse...")
         self.import_button.clicked.connect(self.browse)
         self.filename = None
@@ -41,17 +49,18 @@ class ControlPanel(QWidget):
         self.plot_button = QPushButton("Plot!")
         export_layout.addWidget(self.export_button)
         export_layout.addWidget(self.plot_button)
-        self.layout = QVBoxLayout()
-        self.layout.addLayout(import_layout)
-        self.layout.addLayout(export_layout)
+        layout = QVBoxLayout()
+        layout.addLayout(import_layout)
+        layout.addLayout(export_layout)
+        self.setLayout(layout)
 
     def browse(self):
-        filename, _ = QFileDialog.getOpenFileName(self, "Select tcr file", os.getcwd(),
-                                                  "tcr files(*.tcr)")
+        filename, _ = QFileDialog.getOpenFileName(self, "Select trc file", os.getcwd(),
+                                                  "Tcr files(*.trc)")
         if filename:
             self.filename = filename
             self._parse_tcr_file(self.filename)
 
     def _parse_tcr_file(self, filename):
-        self.import_label.setText(filename)
+        self.import_label.setText(filename.rsplit(SEPARATOR, 1)[1])
         pass
